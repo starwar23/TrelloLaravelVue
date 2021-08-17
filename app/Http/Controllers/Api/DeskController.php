8a21b33742aae7f0ec2171c3305084cc83b1e6b3
Artem <t9_0@bk.ru>
2021-08-18 00:19:34 +0700

    add Requests and delete resource

--- app/Http/Controllers/Api/DeskController.php
+++ app/Http/Controllers/Api/DeskController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\DeskStoreRequest;
+use App\Http\Resources\DeskListResource;
 use App\Http\Resources\DeskResource;
 use App\Models\Desk;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class DeskController extends Controller
 {
@@ -16,7 +19,7 @@ class DeskController extends Controller
      */
     public function index ()
     {
-        return DeskResource::collection(Desk::with('lists')->get());
+        return DeskResource::collection(Desk::all());
     }
 
     /**
@@ -25,9 +28,11 @@ class DeskController extends Controller
      * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function store (Request $request)
+    public function store (DeskStoreRequest $request)
     {
-        //
+        $creared_desk = Desk::create($request->validated());
+
+        return new DeskResource($creared_desk);
     }
 
     /**
@@ -36,9 +41,9 @@ class DeskController extends Controller
      * @param int $id
      * @return \Illuminate\Http\Response
      */
-    public function show ($id)
+    public function show (Desk $desk)
     {
-        return new DeskResource(Desk::with('lists')->findOrFail( $id ));
+        return new DeskResource($desk);
     }
 
     /**
@@ -48,9 +53,11 @@ class DeskController extends Controller
      * @param int $id
      * @return \Illuminate\Http\Response
      */
-    public function update (Request $request, $id)
+    public function update (DeskStoreRequest $request, Desk $desk)
     {
-        //
+        $desk->update($request->validated());
+
+        return new DeskResource($desk);
     }
 
     /**
@@ -59,8 +66,10 @@ class DeskController extends Controller
      * @param int $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy ($id)
+    public function destroy (Desk $desk)
     {
-        //
+        $desk->delete();
+
+        return response(null,Response::HTTP_NO_CONTENT);
     }
 }
